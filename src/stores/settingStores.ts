fd76c7c89cb96f110532495ca94d96e3dafd9baf
Furina de Fontaine <git@daijobudes.net>
2026-08-20 18:58:58 +0800

    feat: rewrite overlay (mania fields, hide circle size when needed)

--- src/stores/settingStores.ts
+++ src/stores/settingStores.ts
@@ -2,8 +2,10 @@ import { atom, map } from "nanostores";
 
 export interface SettingsInterface {
   truncateLength: number;
+  mode: number;
 }
 
 export const $settings = map<SettingsInterface>({
   truncateLength: 0,
+  mode: 0,
 });
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -25,3 +25,14 @@ export function secondsToHumanReadable(totalSeconds: number): string {
 
   return `${minutes}:${paddedSeconds}`;
 }
+
+// Hide circle size if mode is taiko and mania 4k/7k
+export function isCSHidden(): boolean {
+  const mode = $settings.get().mode;
+  return ![1, 3, 4].includes(mode);
+}
+
+export function isMania(): boolean {
+  const mode = $settings.get().mode;
+  return [3, 4].includes(mode);
+}
